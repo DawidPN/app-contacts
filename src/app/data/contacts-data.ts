4cfc3a96468ac5a18b5c5717f669cf4760e879cc
Dawid Poniewierka <dawid.poniewierka@gmail.com>
2023-01-21 12:19:56 +0100

    first commit

---
+++ src/app/data/contacts-data.ts
@@ -0,0 +1,6 @@
+export const CONTACTS = [
+    {id: 1, surname: 'Kowalski', firstName: 'Jan', phoneNumber: '111111111'},
+    {id: 2, surname: 'Kowalska', firstName: 'Anna', phoneNumber: '222222222'},
+    {id: 3, surname: 'Nowak', firstName: 'Maria', phoneNumber: '333333333'},
+    {id: 4, surname: 'Nowakowski', firstName: 'Marek', phoneNumber: '444444444'}
+];
